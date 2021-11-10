b1313acc6707cccec461774aa6dad112e063ddca
qzhao19 <qi.zhao@outlook.fr>
2021-11-10 23:24:34 +0800

    Update filter.py

--- tiny_opencv/filter.py
+++ tiny_opencv/filter.py
@@ -8,7 +8,7 @@ class Filter(object):
     def __init__(self, ksize=3, sigma=1.5):
         self._ksize = ksize
         self._sigma = sigma
-
+       
     def _gaussian_kernel(self):
         """Return a 2d-array of shape [ksize, ksize]
         """
@@ -46,6 +46,7 @@ class Filter(object):
                         dtype=np.float32)
         
         img_out[pad : pad + img_h, pad : pad + img_w, :] = img_in.copy()
+        
         tmp = img_out.copy()
         
         for h in range(img_h):
@@ -53,11 +54,11 @@ class Filter(object):
                 for c in range(img_c):
                     img_out[h + pad, w + pad, c] = np.mean(tmp[h : h + self._ksize, w : w + self._ksize, c])
         
+        
         img_out = np.clip(img_out, 0, 255)
         img_out = img_out[pad : pad + img_h, pad : pad + img_w, :].astype(np.float32)
         
         return img_out
-
 
     def _median_filter(self, img_in):
         """
@@ -91,5 +92,39 @@ class Filter(object):
     
         img_out = np.clip(img_out, 0, 255)
         img_out = img_out[pad : pad + img_h, pad : pad + img_w, :].astype(np.float32) 
+        
+        return img_out
+
+    def _motion_filter(self, img_in):
+        """
+        Parameters
+        ----------
+        img_in : float32 ndarray of shape [height, width, channel]
+            input image.
+
+        Returns
+        -------
+            None.
+        """
+        
+        pad = self.ksize // 2
+        
+        img_h, img_w, img_c = img_in.shape
+        
+        img_out = np.zeros((img_h + pad * 2, img_w + pad * 2, img_c), 
+                           dtype=np.float32)
+        img_out[pad : img_h + pad, pad: img_w + pad, :] = img_in.copy()
+        tmp = img_out.copy()
+        
+        for h in range(img_h):
+            for w in range(img_w):
+                for c in range(img_c):
+                    img_out[h + pad, w + pad, c] = \
+                    np.sum(self.kernel * tmp[h : h + self.ksize, w : w + self.ksize, :])
+        
+        
+        img_out = np.clip(img_out, 0, 255)
+        img_out = img_out[pad : pad + img_h, 
+                          pad : pad + img_w, :].astype(np.float32)
         
         return img_out
