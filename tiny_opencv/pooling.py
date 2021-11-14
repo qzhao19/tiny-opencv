88e4a58244fcec79e8ccc027b2e3d4e4970ef40b
qzhao19 <qi.zhao@outlook.fr>
2021-11-14 23:22:42 +0800

    Update pooling.py

--- tiny_opencv/pooling.py
+++ tiny_opencv/pooling.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-
-def average_pooling(img_in, pool_size=8):
+def _average_pooling(img_in, pool_size=8):
     """
     Parameters
     ----------
@@ -15,7 +14,9 @@ def average_pooling(img_in, pool_size=8):
 
     """
     img_out = img_in.copy()
+    
     img_h, img_w, img_c = img_in.shape
+    
     N_h, N_w = int(img_h / pool_size), int(img_w / pool_size)
     
     for x in range(N_h):
@@ -25,5 +26,33 @@ def average_pooling(img_in, pool_size=8):
                         pool_size * y : pool_size * (y + 1), c] = np.mean(img_out[pool_size * x : pool_size * (x + 1), \
                                                                                   pool_size * y : pool_size * (y + 1), c]).astype(np.int8)
                     
+    return img_out
+
+def _max_pooling(img_in, pool_size=8):
+    """
+        
+    Parameters
+    ----------
+    img_path : TYPE
+        DESCRIPTION.
+    pool_size : TYPE, optional
+        DESCRIPTION. The default is 8.
+
+    Returns
+    -------
+    None.
+    """
+    
+    img_out = img_in.copy()
+    img_h, img_w, img_c = img_in.shape
+    
+    N_h, N_w = int(img_h / pool_size), int(img_w / pool_size)
+    
+    for x in range(N_h):
+        for y in range(N_w):
+            for c in range(img_c):
+                img_out[pool_size * x : pool_size * (x + 1), 
+                        pool_size * y : pool_size * (y + 1), c] = np.max(img_out[pool_size * x : pool_size * (x + 1), 
+                                                                                  pool_size * y : pool_size * (y + 1), c]).astype(np.int8)       
     return img_out
 
